d79b5879707f35852e2c85d254b6b0e9747a8c01
Thabiso Magwaza <magwazathabiso9@gmail.com>
2025-08-03 00:12:39 +0200

    feat: njinji commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const tasks = useQuery(api.tasks.get);
   return (
     <main className="flex flex-col items-center justify-between p-24 gap-4">
-      <h1 className="text-2xl font-bold">Tasks</h1>
+      <h1 className="text-2xl font-bold">Tasks - {tasks?.length}</h1>
       <ol className="list-decimal list-inside">
         {tasks?.map(({ _id, text }) => <li key={_id}>{text}</li>)}
       </ol>
