709ea6694e5f937aa1b3024d581faa6cd2042cb7
Thabiso Magwaza <magwazathabiso9@gmail.com>
2025-08-17 10:25:27 +0200

    feat: installed shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
-'use client';
-
-import { useQuery } from 'convex/react';
-import { api } from '../../convex/_generated/api';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
-  const tasks = useQuery(api.tasks.get);
   return (
     <main className="flex flex-col items-center justify-between p-24 gap-4">
-      <h1 className="text-2xl font-bold">Tasks - {tasks?.length}</h1>
-      <ol className="list-decimal list-inside">
-        {tasks?.map(({ _id, text }) => <li key={_id}>{text}</li>)}
-      </ol>
+      <Button variant="outline">Click me</Button>
     </main>
   );
 }
